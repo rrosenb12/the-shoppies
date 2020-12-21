1f12c6f795c8657e4d94e7e380915870b463cfb1
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-12-21 14:30:40 -0500

    basically done

--- src/App.js
+++ src/App.js
@@ -1,38 +1,53 @@
-import './App.css';
-import Search from "./Search"
-import ResultsContainer from "./ResultsContainer"
-import NomineesContainer from "./NomineesContainer"
-import React from "react"
-import {connect} from "react-redux"
+import "./App.css";
+import Search from "./Search";
+import ResultsContainer from "./ResultsContainer";
+import NomineesContainer from "./NomineesContainer";
+import React from "react";
+import { connect } from "react-redux";
+import Result from "./Result";
 
 class App extends React.Component {
-
   state = {
-    searchTerm: ""
-  }
-  
+    searchTerm: "",
+  };
+
   handleSearch = (searchTerm) => {
     this.setState({
-      searchTerm: searchTerm
-    })
-  }
+      searchTerm: searchTerm,
+    });
+  };
 
-  render(){
-  return (
-    <div className="app">
-      <div className="top">
-        <h1 className="title-header">The Shoppies</h1>
-        <Search handleSearch={this.handleSearch}/>
+  render() {
+    return (
+      <div className="app">
+        {this.props.nominees.length === 5 && (
+          <div className="fade">
+            <div className="banner">
+              <p className="message">you have reached your nominee limit</p>
+              <p className="message">
+                please un-nominate a movie
+              </p>
+              <div className="banner-results">
+                {this.props.nominees.map((nominee) => (
+                  <Result type="nominee" key={nominee.imbdID} movie={nominee} />
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="top">
+          <h1 className="title-header">The Shoppies</h1>
+          <Search handleSearch={this.handleSearch} />
+        </div>
+        <div className="main-body">
+          <ResultsContainer searchTerm={this.state.searchTerm} />
+          <NomineesContainer />
+        </div>
       </div>
-      {this.props.nominees.length === 5 && <div className="banner">you have reached your nominee limit</div>}
-      <div className="main-body">
-        <ResultsContainer searchTerm={this.state.searchTerm}/>
-        <NomineesContainer />
-      </div>
-    </div>
-  )};
+    );
+  }
 }
 
-const mapStateToProps = state => ({nominees: state.nominatedMovies})
+const mapStateToProps = (state) => ({ nominees: state.nominatedMovies });
 
 export default connect(mapStateToProps)(App);
--- src/Search.js
+++ src/Search.js
@@ -1,36 +1,44 @@
-import React from "react"
-import {addMoviesToMainContainer} from "./movieActions"
-import {connect} from "react-redux"
+import React from "react";
+import { addMoviesToMainContainer } from "./movieActions";
+import { connect } from "react-redux";
 
-class Search extends React.Component{
-    state={
-        searchTerm:""
-    }
+class Search extends React.Component {
+  state = {
+    searchTerm: "",
+  };
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        fetch(`http://www.omdbapi.com/?s=${this.state.searchTerm}&apikey=38d091f`)
-        .then(response => response.json())
-        .then(response => {
-            this.props.addMoviesToMainContainer(response.Search)
-            this.props.handleSearch(this.state.searchTerm)
-        })
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`http://www.omdbapi.com/?s=${this.state.searchTerm}&apikey=38d091f`)
+      .then((response) => response.json())
+      .then((response) => {
+        response.Response === "False"
+          ? window.alert("please enter a different search term")
+          : this.props.addMoviesToMainContainer(response.Search);
+        this.props.handleSearch(this.state.searchTerm);
+      });
+  };
 
-    render(){
-        return(
-            <form className="search-bar" onSubmit={this.handleSubmit}>
-                <input type="text" name="searchTerm" value={this.state.searchTerm} onChange={this.handleChange} placeholder="Enter a Movie Title"/>
-                <input type="submit" value="Search"/>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form className="search-bar" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="searchTerm"
+          value={this.state.searchTerm}
+          onChange={this.handleChange}
+          placeholder="Enter a Movie Title"
+        />
+        <input type="submit" value="Search" />
+      </form>
+    );
+  }
 }
 
-export default connect(null, {addMoviesToMainContainer})(Search)
+export default connect(null, { addMoviesToMainContainer })(Search);
